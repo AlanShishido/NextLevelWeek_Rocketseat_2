7de3ebd50ded1a5f29dd1073bc9b3dcee96b044f
AlanShishido <51389025+AlanShishido@users.noreply.github.com>
2020-08-06 18:55:00 -0400

    third day

--- src/components/pageHeader/index.tsx
+++ src/components/pageHeader/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg'
 import backIcon from '../../assets/images/icons/back.svg'
@@ -8,24 +8,26 @@ import './styles.css'
 
 interface PageHeaderProps {
     title: string;
+    description?: string,
 }
 
-const PageHeader:React.FC<PageHeaderProps> = (props) => {
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
     return (
         <header className="page-header">
-        <div className="top-bar-container">
-            <Link to="/">
-                <img src={backIcon} alt="Voltar"/>
-            </Link>
-            <img src={logoImg} alt="Logomarca"/>
-        </div>
+            <div className="top-bar-container">
+                <Link to="/">
+                    <img src={backIcon} alt="Voltar" />
+                </Link>
+                <img src={logoImg} alt="Logomarca" />
+            </div>
 
-        <div className="header-content">
-            <strong>{props.title}</strong>
-            {props.children}
-        </div>
+            <div className="header-content">
+                <strong>{props.title}</strong>
+                {props.description && <p>{props.description}</p>}
+                {props.children}
+            </div>
 
-    </header>
+        </header>
     );
 }
 
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 import PageHeader from '../../components/pageHeader';
 import TeacherItem from '../../components/teacherItems';
+import Input from '../../components/input';
 
 import './styles.css'
 
@@ -11,43 +12,34 @@ function TeacherList() {
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject"/>
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="week-day">Dia da Semana</label>
-                        <input type="text" id="week-day"/>
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="time">Horário</label>
-                        <input type="text" id="time"/>
-                    </div>
+                    <Input name="subject" label="Matéria"/>
+                    <Input name="week-day" label="Dia da semana"/>
+                    <Input name="time" label="Horário" type="time"/>
                 </form>
             </PageHeader>
 
             <main>
-                <TeacherItem 
+                <TeacherItem
                     imgURL="https://avatars2.githubusercontent.com/u/51389025?s=460&u=cd50b629b6fb131d354e283a7b1ee85b4f32e814&v=4"
-                    teacherName = "Alan Shishido"
-                    teaching = "Automação Industrial"    
-                    about = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dolores dignissimos temporibus rerum! Minima cum dignissimos earum eaque illo excepturi obcaecati pariatur. In illum temporibus vero veritatis libero. Cumque, recusandae."
-                    price = "R$ 80,00"
+                    teacherName="Alan Shishido"
+                    teaching="Automação Industrial"
+                    about="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dolores dignissimos temporibus rerum! Minima cum dignissimos earum eaque illo excepturi obcaecati pariatur. In illum temporibus vero veritatis libero. Cumque, recusandae."
+                    price="R$ 80,00"
                 />
-                
-                <TeacherItem 
-                    imgURL="https://avatars0.githubusercontent.com/u/23108122?s=460&u=3ea6fce9e3b7373eade3f03fc62d8a6d5de30b20&v=4" 
-                    teacherName = "Leonardo Goulart"
-                    teaching = "Programação Front-End"    
-                    about = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dolores dignissimos temporibus rerum! Minima cum dignissimos earum eaque illo excepturi obcaecati pariatur. In illum temporibus vero veritatis libero. Cumque, recusandae."
-                    price = "R$ 60,00"
+
+                <TeacherItem
+                    imgURL="https://avatars0.githubusercontent.com/u/23108122?s=460&u=3ea6fce9e3b7373eade3f03fc62d8a6d5de30b20&v=4"
+                    teacherName="Leonardo Goulart"
+                    teaching="Programação Front-End"
+                    about="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dolores dignissimos temporibus rerum! Minima cum dignissimos earum eaque illo excepturi obcaecati pariatur. In illum temporibus vero veritatis libero. Cumque, recusandae."
+                    price="R$ 60,00"
                 />
-                <TeacherItem 
+                <TeacherItem
                     imgURL="https://avatars3.githubusercontent.com/u/8538371?s=460&u=103197e6aff9fd31bb0181bd2d0744d885c77258&v=4"
-                    teacherName = "Bianca Santiago"
-                    teaching = "PRogramação Back-End"    
-                    about = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dolores dignissimos temporibus rerum! Minima cum dignissimos earum eaque illo excepturi obcaecati pariatur. In illum temporibus vero veritatis libero. Cumque, recusandae."
-                    price = "R$ 100,00"
+                    teacherName="Bianca Santiago"
+                    teaching="PRogramação Back-End"
+                    about="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dolores dignissimos temporibus rerum! Minima cum dignissimos earum eaque illo excepturi obcaecati pariatur. In illum temporibus vero veritatis libero. Cumque, recusandae."
+                    price="R$ 100,00"
                 />
             </main>
         </div>
