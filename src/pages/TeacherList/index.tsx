2bdadde50e4837638adbeb57df96a4c386bd02eb
AlanShishido <51389025+AlanShishido@users.noreply.github.com>
2020-08-07 19:25:19 -0400

    quird day

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,20 +1,68 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 
 import PageHeader from '../../components/pageHeader';
 import TeacherItem from '../../components/teacherItems';
 import Input from '../../components/input';
+import Select from '../../components/Select';
+import api from '../../services/api';
 
 import './styles.css'
 
 function TeacherList() {
+    const [subject, setSubject] = useState('')
+    const [week_day, setWeekDay] = useState('')
+    const [time, setTime] = useState('')
+
+    function searchTeachers(e: FormEvent){
+        e.preventDefault();
+
+        api.post('acess','');
+    }
+
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
-                <form id="search-teachers">
-                    <Input name="subject" label="Matéria"/>
-                    <Input name="week-day" label="Dia da semana"/>
-                    <Input name="time" label="Horário" type="time"/>
+                <form id="search-teachers" onSubmit={searchTeachers}>
+                    <Select
+                        name="subject"
+                        label="Matéria"
+                        value={subject}
+                        onChange={(e) => { setSubject(e.target.value) }}
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Ciências', label: 'Ciências' },
+                            { value: 'Educação física', label: 'Educação física' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Química', label: 'Química' },
+                            { value: 'História', label: 'História' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Inglês', label: 'Inglês' },
+                            { value: 'Espanhol', label: 'Espanhol' },
+                        ]}
+                    />
+                    <Select
+                        name="week_day"
+                        label="Dia da semana"
+                        value={week_day}
+                        onChange={(e) => { setWeekDay(e.target.value) }}
+                        options={[
+                            { value: '0', label: 'Domingo' },
+                            { value: '1', label: 'Segunda-feira' },
+                            { value: '2', label: 'Terça-feira' },
+                            { value: '3', label: 'Quarta-feira' },
+                            { value: '4', label: 'Quinta-feira' },
+                            { value: '5', label: 'Sexta-feira' },
+                            { value: '6', label: 'Sábado' },
+                        ]}
+                    />
+                    <Input name="time" label="Horário" type="time" value="time" onChange={(e) => { setTime(e.target.value) }} />
+
+
+                    <button type="submit">Buscar</button>
                 </form>
             </PageHeader>
 
